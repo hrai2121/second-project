769afff9ab0723f0fffc41af20e00f85a800078d
Harsh <harshrai0137@gmail.com>
2024-05-09 14:13:29 +0530

    ahflkasdf

--- New7/src/structt/Devv2.java
+++ New7/src/structt/Devv2.java
@@ -4,7 +4,10 @@ public class Devv2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("developer2");
+		
+		String h="harsh";
+		System.out.println("developer2"+h);
+		
 
 	}
 
